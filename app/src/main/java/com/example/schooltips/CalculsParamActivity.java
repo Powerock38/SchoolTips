8e0ca7f20b4e2a487f80038f8d386bd16b0d823b
Powerock38 <mat.papp@free.fr>
2021-03-23 18:11:16 +0100

    CalculsParam

---
+++ app/src/main/java/com/example/schooltips/CalculsParamActivity.java
@@ -0,0 +1,14 @@
+package com.example.schooltips;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculsParamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculs_param);
+    }
+}
