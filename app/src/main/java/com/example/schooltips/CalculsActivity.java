0c0a1268f007bf7b98546638c23549a4dfcfdef6
Powerock38 <mat.papp@free.fr>
2021-03-30 15:05:41 +0200

    Calculs

--- app/src/main/java/com/example/schooltips/CalculsActivity.java
+++ app/src/main/java/com/example/schooltips/CalculsActivity.java
@@ -1,21 +1,27 @@
 package com.example.schooltips;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import static java.lang.Integer.parseInt;
 
 public class CalculsActivity extends AppCompatActivity {
     static public final String OPERATION_KEY = "OPERATION_KEY";
     static public final String FORMAT_A_KEY = "FORMAT_A_KEY";
     static public final String FORMAT_B_KEY = "FORMAT_B_KEY";
+    static public final String NB_QUESTION_KEY = "NB_QUESTION_KEY";
 
-    private int formatA;
-    private int formatB;
-    private Operation operation;
+    private String op;
+    private Calculs calculs;
+    private TextView question;
+    private EditText answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +29,55 @@ public class CalculsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calculs);
 
         Intent intent = getIntent();
-        formatA = intent.getIntExtra(FORMAT_A_KEY, 1);
-        formatB = intent.getIntExtra(FORMAT_B_KEY, 1);
-        operation = (Operation) intent.getSerializableExtra(OPERATION_KEY);
+        int formatA = intent.getIntExtra(FORMAT_A_KEY, 1);
+        int formatB = intent.getIntExtra(FORMAT_B_KEY, 1);
+        Operation operation = (Operation) intent.getSerializableExtra(OPERATION_KEY);
+        int nbQuestions = intent.getIntExtra(NB_QUESTION_KEY, 1);
 
-        TextView question = findViewById(R.id.question);
-        EditText answer = findViewById(R.id.answer);
-        Button next = findViewById(R.id.next);
+        Toast.makeText(this, "operation : " + operation.name(), Toast.LENGTH_SHORT).show();
 
-        Calculs calculs = new Calculs(formatA, formatB, operation);
+        question = findViewById(R.id.question);
+        answer = findViewById(R.id.answer);
+
+        calculs = new Calculs(formatA, formatB, operation, nbQuestions);
+
+        switch (operation) {
+            case subtract:
+                op = getString(R.string.calculs_op_moins);
+                break;
+            case multiply:
+                op = getString(R.string.calculs_op_mul);
+                break;
+            case divide:
+                op = getString(R.string.calculs_op_div);
+                break;
+            default: // add
+                op = getString(R.string.calculs_op_plus);
+        }
+
+        nextCalculView(null);
+    }
+
+    public void nextCalculView(View v) {
+        boolean shouldAskMore;
+        if (v == null) {
+            shouldAskMore = calculs.nextCalcul(null);
+        } else {
+            int rep;
+            try {
+                rep = parseInt(answer.getText().toString());
+            } catch (NumberFormatException e) {
+                rep = 0;
+            }
+            shouldAskMore = calculs.nextCalcul(rep);
+        }
+
+        if (shouldAskMore) {
+            answer.getText().clear();
+            question.setText(getString(R.string.calculs, calculs.getOp1(), op, calculs.getOp2()));
+        } else {
+            Toast.makeText(this, "vous avez " + calculs.getBonnesRep() + " bonnes réponses", Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 }
--- app/src/main/java/com/example/schooltips/CalculsParamActivity.java
+++ app/src/main/java/com/example/schooltips/CalculsParamActivity.java
@@ -2,17 +2,19 @@ package com.example.schooltips;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CalculsParamActivity extends AppCompatActivity {
-    private int formatA;
-    private int formatB;
-    private Operation operation;
+    private int formatA = 1;
+    private int formatB = 1;
+    private Operation operation = Operation.add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +22,8 @@ public class CalculsParamActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calculs_param);
 
         NumberPicker nbPickerNbOp = findViewById(R.id.nb_op);
-        String[] possibleSeries = {"5", "10", "15", "20"};
-        nbPickerNbOp.setDisplayedValues(possibleSeries);
+        nbPickerNbOp.setMinValue(1); //5
+        nbPickerNbOp.setMaxValue(20);
 
         RadioGroup formatAGroup = findViewById(R.id.format_a);
         formatAGroup.setOnClickListener(new View.OnClickListener() {
@@ -88,6 +90,7 @@ public class CalculsParamActivity extends AppCompatActivity {
                 intent.putExtra(CalculsActivity.FORMAT_A_KEY, formatA);
                 intent.putExtra(CalculsActivity.FORMAT_B_KEY, formatB);
                 intent.putExtra(CalculsActivity.OPERATION_KEY, operation);
+                intent.putExtra(CalculsActivity.NB_QUESTION_KEY, nbPickerNbOp.getValue());
                 startActivity(intent);
             }
         });
