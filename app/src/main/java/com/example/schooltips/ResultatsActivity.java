305fb4c7fe12af25e839966bb4985700b04a558b
TanSinoret <tanguysignoret@gmail.com>
2021-04-07 20:15:35 +0200

    MAJ c'est bon

--- app/src/main/java/com/example/schooltips/ResultatsActivity.java
+++ app/src/main/java/com/example/schooltips/ResultatsActivity.java
@@ -1,12 +1,17 @@
 package com.example.schooltips;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class ResultatsActivity extends AppCompatActivity {
     static public final String NB_QUESTIONS_KEY = "NB_QUESTIONS_KEY";
@@ -14,14 +19,24 @@ public class ResultatsActivity extends AppCompatActivity {
     static public final String LISTE_ERREURS_KEY = "LISTE_ERREURS_KEY";
     static public final String OPERATION_KEY = "OPERATION_KEY";
 
+    private int nbQuestions;
+    private User user;
+
+    // Database
+    private DatabaseUser userDataBase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultats);
 
+        // Instanciation de la BDD
+        userDataBase = DatabaseUser.getInstance(getApplicationContext());
+        user =  ((MyApplication) getApplication()).getUser();
+
         Intent intent = getIntent();
 
-        int nbQuestions = intent.getIntExtra(NB_QUESTIONS_KEY, 0);
+        nbQuestions = intent.getIntExtra(NB_QUESTIONS_KEY, 0);
         int nbErreurs = intent.getIntExtra(NB_ERREURS_KEY, 0);
 
         Button retourExos = findViewById(R.id.retour_exos);
@@ -30,8 +45,6 @@ public class ResultatsActivity extends AppCompatActivity {
         TextView res_xsurx = findViewById(R.id.resultat_xsurx);
 
         res_xsurx.setText(getString(R.string.xsurx, nbQuestions - nbErreurs, nbQuestions));
-
-        
 
         if (nbErreurs > 0) {
             res.setText(getResources().getQuantityString(R.plurals.erreurs, nbErreurs));
@@ -46,6 +59,8 @@ public class ResultatsActivity extends AppCompatActivity {
                 }
             });
         } else {
+            majUser();
+
             res.setText(getString(R.string.all_good));
             corriger.setVisibility(View.GONE);
         }
@@ -58,5 +73,26 @@ public class ResultatsActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void majUser() {
+        class MajUser extends  AsyncTask<Void, Void, Void> {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                userDataBase.getAppDatabase()
+                        .userDao()
+                        .majHighScore(user.getId(), user.getHighScore());
+                return null;
+            }
+        }
+
+        //////////////////////////
+        // MAJ du user dans MyApplication
+        ((MyApplication) getApplication()).majUser(nbQuestions, getApplicationContext());
+
+        // Maj du USer dans la BDD
+        MajUser maj = new MajUser();
+        maj.execute();
     }
 }
